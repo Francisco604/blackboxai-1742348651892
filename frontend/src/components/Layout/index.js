ce6a39d843a1b23963515fba8878d9fb5b7dcdee
Francisco604 <francisco.madureira.22213@meu.ipta.pt>
2025-03-19 01:44:27 +0000

    Add frontend/src/components/Layout/index.js 1742348667273

---
+++ frontend/src/components/Layout/index.js
@@ -0,0 +1,7 @@
+import Layout from './Layout';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+export { Header, Sidebar, Footer };
+export default Layout;
